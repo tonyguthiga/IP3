296f45a9d5f0f88b6eb6a530bc1edf1663bddd20
antony-guthiga <toniguthiga93@gmail.com>
2020-05-10 17:28:55 +0300

    combined function in what to do section

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,17 @@
 $(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".design-showing").toggle();
-    $(".design-hidden").toggle();
-  });
+    $(".we").click(function(){
+        $(this).children(".hide").slideToggle(1500);
+        $(this).children(".show").toggle(1500);
+    });
 });
 
-$(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".develop-showing").toggle();
-    $(".develop-hidden").toggle();
-  });
-});
 
-$(document).ready(function() {
-  $(".clickable").click(function() {
-    $(".product-showing").toggle();
-    $(".develop-hidden").toggle();
-  });
-});
+$(document).ready(function(){
+    $(".space").hover(function(){
+        $("#main").fadeOut("slow");
+        $("#overlay").fadeIn("Space App");
+    }, function(){
+        $("#overlay").fadeOut("Space App");
+        $("#main").fadeIn("slow");
+    });
+})
